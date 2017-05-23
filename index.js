cc5cc886d455beaf81648731c90849fb89bbd93d
eda013 <poukupenga@gmail.com>
2017-05-24 10:22:20 +1200

    Working basic api search

---
+++ index.js
@@ -0,0 +1,19 @@
+var request = require('superagent')
+var url = 'https://igdbcom-internet-game-database-v1.p.mashape.com'
+
+function getGames(callback, searchStr) {
+  request
+    .get(`${url}/games/?fields=name&limit=10&offset=0&order=release_dates.date%3Adesc&search=${searchStr}`)
+    .set('X-Mashape-Key', '8XrLHLkRxlmshascF7n3mXc7CtoVp1RQN3Yjsn4ISq8ddFmTDT')
+    .set('Accept', 'application/json')
+    .end(function(err, res) {
+      if(err || !res.ok) {
+        alert('Oh no error!')
+    } else {
+     callback(res.body)
+     callback(res.ok)
+    }
+  })
+}
+
+getGames(console.log, 'halo')
